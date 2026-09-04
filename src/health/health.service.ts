ac2d1db72ccf54033db5ea0834d27a24443d230d
Altamash Khan <altamashidrisi9090@gmail.com>
2026-09-05 00:04:50 +0530

    feat: built an health module

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class HealthService {}
+export class HealthService {
+    getHealth() {
+        return {
+            status: "ok",
+            service: "mirova-backend",
+        };
+    }
+}
